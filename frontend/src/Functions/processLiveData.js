0074056a5d39d62522dcaf1721e401efcc7c95b6
Lakshya Gupta <lgupta@treyarch.com>
2024-04-21 11:11:21 -0700

    Finished updating front-end to handle data via websockets

--- frontend/src/Functions/processLiveData.js
+++ frontend/src/Functions/processLiveData.js
@@ -1,17 +1,25 @@
-const MAX_SAVED_FRAME_NUMBERS = 64
 
 export class Queue {
     /**
-     * @param {{ packetParentName: string, packetName: string }} dataSource
+     * @param {{ packetID: number, packetName: string }} dataSource
+     * @param {function} newDataCallback 
      */
-    constructor(dataSource) {
+    constructor(dataSource, newDataCallback) {
         this.items = []
         this.enqueuedFrameNums = new Set()
         this.dataSource = dataSource
+        this.newDataCallback = newDataCallback
+    }
+
+    setNewDataCallback(callbackFn)
+    {
+        this.newDataCallback = callbackFn;
+        console.log(`Registered new data callback for ${this.dataSource.packetName}`);
     }
 
     enqueue(element) {
         this.items.push(element)
+        this.newDataCallback && this.newDataCallback();
     }
 
     dequeue() {
@@ -32,88 +40,27 @@ export class Queue {
 }
 
 /**
- * @param {{ [key: string]: Queue }} dataQueues
- */
-export async function fetchTelemetryData(dataQueues) {
-    try {
-        const response = await fetch("/api/live")
-        if (!response.ok) {
-            throw new Error("Failed to fetch telemetry data")
-        }
-        const telemetryData = await response.json()
-
-        Object.keys(dataQueues).forEach((key) => {
-            const queue = dataQueues[key]
-
-            for (
-                let i = 0;
-                i < telemetryData[queue.dataSource.packetParentName].length;
-                i++
-            ) {
-                const frame =
-                    telemetryData[queue.dataSource.packetParentName][i]
-                const overallFrameIdentifier =
-                    frame.Header.OverallFrameIdentifier
-                if (!queue.enqueuedFrameNums.has(overallFrameIdentifier)) {
-                    queue.enqueuedFrameNums.add(overallFrameIdentifier)
-                    if (
-                        queue.enqueuedFrameNums.size > MAX_SAVED_FRAME_NUMBERS
-                    ) {
-                        const oldestFrameNumber = Math.min(
-                            ...[...queue.enqueuedFrameNums.values()]
-                        )
-                        queue.enqueuedFrameNums.delete(oldestFrameNumber)
-                    }
-
-                    const playerIndex = frame.Header.PlayerCarIndex
-                    const playerData =
-                        frame.Body[queue.dataSource.packetName][playerIndex]
-
-                    queue.enqueue(playerData)
-                }
-            }
-        })
-    } catch (error) {
-        console.error("Error fetching telemetry data:", error)
-    }
-}
-
-/**
  * @param {Object} packet
  * @param {{ [key: string]: Queue }} dataQueues
  */
-function HandleNewPacket(packet, dataQueues) {
-    Object.keys(dataQueues).forEach((key) => {
-        // TODO: fix this, it's not ready for use
-        const queue = dataQueues[key]
-        const frame = telemetryData[queue.dataSource.packetParentName][i]
-        const overallFrameIdentifier =
-            frame.Header.OverallFrameIdentifier
-        if (!queue.enqueuedFrameNums.has(overallFrameIdentifier)) {
-            queue.enqueuedFrameNums.add(overallFrameIdentifier)
-            if (
-                queue.enqueuedFrameNums.size > MAX_SAVED_FRAME_NUMBERS
-            ) {
-                const oldestFrameNumber = Math.min(
-                    ...[...queue.enqueuedFrameNums.values()]
-                )
-                queue.enqueuedFrameNums.delete(oldestFrameNumber)
-            }
-
-            const playerIndex = frame.Header.PlayerCarIndex
-            const playerData =
-                frame.Body[queue.dataSource.packetName][playerIndex]
-
-            queue.enqueue(playerData)
+function HandleNewPacket(packet, dataQueues)
+{
+    Object.keys(dataQueues).forEach((key) =>
+    {
+        const queue = dataQueues[key];
+        if (queue.dataSource.packetID === packet.Header.PacketId)
+        {
+            queue.enqueue(packet);
+            return;
         }
-    })
+    });
 }
 
 /**
  * @param {{ [key: string]: Queue }} dataQueues
  */
 export async function SubscribeToBackend(dataQueues) {
-    const ws = new WebSocket(`ws://${window.location.host}/api/live`);
+    const ws = new WebSocket(`ws://localhost:8000/api/live`);
     ws.onopen = function (ev) {
         console.log("Backend connection success");
     }
--- frontend/src/Pages/LiveData.js
+++ frontend/src/Pages/LiveData.js
@@ -1,168 +1,177 @@
-import React, { useState, useEffect } from "react"
-import { Row, Col } from "react-bootstrap"
-import { TotalTelemetryChart } from "../Components/ChartComponents"
-import { TyreChart } from "../Components/TyreChart"
-import { TyreChartAlternate } from "../Components/TyreChartAlternate"
+import React, { useState, useEffect, useContext } from "react";
+import { Row, Col } from "react-bootstrap";
+import { TotalTelemetryChart } from "../Components/ChartComponents";
+import { TyreChart } from "../Components/TyreChart";
+import { TyreChartAlternate } from "../Components/TyreChartAlternate";
 import {
-    SteeringIndicator,
-    NumberChart,
-    PitChart,
-    RPMIndicator,
-    SpeedChart,
-    FuelChart,
-    LiveBestLapTimes,
-    LapDetailsTracker,
-} from "../Components/LiveDataCharts"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { dummyTelemetryData } from "../Data/telemetryData"
-import { generateRandomTelemetryData } from "../Functions/telemetryUtils"
-import { fetchTelemetryData, Queue } from "../Functions/processLiveData"
+	SteeringIndicator,
+	NumberChart,
+	PitChart,
+	RPMIndicator,
+	SpeedChart,
+	FuelChart,
+	LiveBestLapTimes,
+	LapDetailsTracker,
+} from "../Components/LiveDataCharts";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { dummyTelemetryData } from "../Data/telemetryData";
+import { generateRandomTelemetryData } from "../Functions/telemetryUtils";
+import {
+	fetchTelemetryData,
+	Queue,
+	SubscribeToBackend,
+} from "../Functions/processLiveData";
+
+const F1PacketID = {
+	Motion: 0,
+	Session: 1,
+	LapData: 2,
+	Event: 3,
+	Participants: 4,
+	CarSetups: 5,
+	CarTelemetry: 6,
+	CarStatus: 7,
+	FinalClassification: 8,
+	LobbyInfo: 9,
+	CarDamage: 10,
+	SessionHistory: 11,
+	TyreSets: 12,
+	MotionEx: 13,
+};
 
 const dataQueues = {
-    carTelemetryDataQueue: new Queue({
-        packetParentName: "CarTelemetryDataPackets",
-        packetName: "CarTelemetryData",
-    }),
-    carStatusDataQueue: new Queue({
-        packetParentName: "CarStatusPackets",
-        packetName: "CarStatusData",
-    }),
-    lapDataQueue: new Queue({
-        packetParentName: "LapDataPackets",
-        packetName: "LapData",
-    }),
+	carTelemetryDataQueue: new Queue({
+		packetID: F1PacketID.CarTelemetry,
+		packetName: "CarTelemetryData",
+	}),
+	carStatusDataQueue: new Queue({
+		packetID: F1PacketID.CarStatus,
+		packetName: "CarStatusData",
+	}),
+	lapDataQueue: new Queue({
+		packetID: F1PacketID.LapData,
+		packetName: "LapData",
+	}),
+};
+
+/**
+ * @param {Queue} queue
+ * @param {function} setFrameFunc
+ */
+function NewPacket(queue, setFrameFunc) {
+	const frame = queue.dequeue();
+	const packetName = queue.dataSource.packetName;
+
+	function AddNewFrame(frameList, newFrame, packetName) {
+		const newDataList = [
+			...frameList,
+			newFrame.Body[packetName][newFrame.Header.PlayerCarIndex],
+		];
+		if (newDataList.length > 150) {
+			newDataList.splice(0, newDataList.length - 150); // keep only the last 80 elements
+		}
+
+		return newDataList;
+	}
+
+	if (frame != null) {
+		setFrameFunc((prevList) => {
+			return AddNewFrame(prevList, frame, packetName);
+		});
+	}
 }
 
 export const LiveData = () => {
-    const [selectedSession, setSelectedSession] = useState("Race")
-    const [selectedTrack, setSelectedTrack] = useState("Track 1")
-    const [selectedLap, setSelectedLap] = useState(1)
-    const [carTelemetryData, setCarTelemetryData] = useState([])
-    const [carStatusData, setCarStatusData] = useState([])
-    const [lapData, setLapData] = useState([])
-    const [cursorX, setCursorX] = useState(null)
+	const [selectedSession, setSelectedSession] = useState("Race");
+	const [selectedTrack, setSelectedTrack] = useState("Track 1");
+	const [selectedLap, setSelectedLap] = useState(1);
+	const [carTelemetryData, setCarTelemetryData] = useState([]);
+	const [carStatusData, setCarStatusData] = useState([]);
+	const [lapData, setLapData] = useState([]);
+	const [cursorX, setCursorX] = useState(null);
 
-    const handleCursorMove = (e) => {
-        if (e && e.activePayload && e.activePayload[0]) {
-            setCursorX(e.activePayload[0].payload.distance)
-        }
-    }
-    // useEffect(() => {
-    //     setTelemetryData(dummyTelemetryData.telemetry)
-    // }, [selectedSession, selectedTrack, selectedLap])
+	const handleCursorMove = (e) => {
+		if (e && e.activePayload && e.activePayload[0]) {
+			setCursorX(e.activePayload[0].payload.distance);
+		}
+	};
 
-    useEffect(() => {
-        dataQueues.carTelemetryDataQueue.reset()
-        const intervalId = setInterval(
-            () => fetchTelemetryData(dataQueues),
-            100
-        )
-        const dequeueInterval = setInterval(() => {
-            const newFrames = {
-                newCarTelemetryFrame: {
-                    frame: dataQueues.carTelemetryDataQueue.dequeue(),
-                    setFrameFunc: setCarTelemetryData,
-                },
-                newCarStatusFrame: {
-                    frame: dataQueues.carStatusDataQueue.dequeue(),
-                    setFrameFunc: setCarStatusData,
-                },
-                newLapDataFrame: {
-                    frame: dataQueues.lapDataQueue.dequeue(),
-                    setFrameFunc: setLapData,
-                },
-            }
+	useEffect(() => {
+		dataQueues.carTelemetryDataQueue.setNewDataCallback(() => NewPacket(dataQueues.carTelemetryDataQueue, setCarTelemetryData));
+		dataQueues.carStatusDataQueue.setNewDataCallback(() => NewPacket(dataQueues.carStatusDataQueue, setCarStatusData));
+		dataQueues.lapDataQueue.setNewDataCallback(() => NewPacket(dataQueues.lapDataQueue, setLapData));
 
-            function AddNewFrame(frameList, newFrame) {
-                const newDataList = [...frameList, newFrame]
-                if (newDataList.length > 150) {
-                    newDataList.splice(0, newDataList.length - 150) // keep only the last 80 elements
-                }
-                return newDataList
-            }
+		SubscribeToBackend(dataQueues);
 
-            Object.keys(newFrames).forEach((key) => {
-                const { frame, setFrameFunc } = newFrames[key]
-                frame !== null &&
-                    setFrameFunc((prevList) => {
-                        return AddNewFrame(prevList, frame)
-                    })
-            })
-        }, 8)
+		return () => {
+		}; // Cleanup function
+	}, []);
 
-        return () => {
-            clearInterval(intervalId)
-            clearInterval(dequeueInterval)
-        } // Cleanup function
-    }, [])
-    // console.log(
-    //     telemetryData[telemetryData.length - 1]["TyresSurfaceTemperature"]
-    // )
-    return (
-        <div
-            style={{
-                background: "#202020", //Gunmetal: 2C3539, 202020, 232c30, 262f33// DarkGrey: 1C1C1C
-                fontFamily: "Futura",
-                color: "white",
-                position: "relative",
-            }}
-        >
-            <Row>
-                <Col>
-                    <div>
-                        {carTelemetryData.length >= 1 ? (
-                            <>
-                                <TotalTelemetryChart
-                                    data={carTelemetryData}
-                                    syncId="telemetryCharts"
-                                    onMouseMove={handleCursorMove}
-                                    throttleKey="Throttle"
-                                    brakeKey="Brake"
-                                    titleLabel="Telemetry v Distance"
-                                />
+	return (
+		<div
+			style={{
+				background: "#202020", //Gunmetal: 2C3539, 202020, 232c30, 262f33// DarkGrey: 1C1C1C
+				fontFamily: "Futura",
+				color: "white",
+				position: "relative",
+			}}
+		>
+			<Row>
+				<Col>
+					<div>
+						{carTelemetryData.length >= 1 ? (
+							<>
+								<TotalTelemetryChart
+									data={carTelemetryData}
+									syncId="telemetryCharts"
+									onMouseMove={handleCursorMove}
+									throttleKey="Throttle"
+									brakeKey="Brake"
+									titleLabel="Telemetry v Distance"
+								/>
 
-                                <Row
-                                //  style={{ border: "1px solid magenta" }}
-                                >
-                                    <Col xs={6} style={{}}>
-                                        <RPMIndicator
-                                            x={200}
-                                            y={120}
-                                            rpm={
-                                                carTelemetryData[
-                                                    carTelemetryData.length - 1
-                                                ].EngineRPM
-                                            }
-                                            gear={
-                                                carTelemetryData[
-                                                    carTelemetryData.length - 1
-                                                ].Gear
-                                            }
-                                        />
-                                    </Col>
-                                    <Col
-                                        xs={6}
-                                        // style={{ border: "1px solid blue" }}
-                                    >
-                                        <SpeedChart
-                                            x={80}
-                                            y={60}
-                                            value={
-                                                carTelemetryData[
-                                                    carTelemetryData.length - 1
-                                                ].Speed
-                                            }
-                                        />
-                                        <SteeringIndicator
-                                            value={
-                                                carTelemetryData[
-                                                    carTelemetryData.length - 1
-                                                ].Steer
-                                            }
-                                        />
-                                    </Col>
-                                </Row>
-                                {/* <NumberChart
+								<Row
+								//  style={{ border: "1px solid magenta" }}
+								>
+									<Col xs={6} style={{}}>
+										<RPMIndicator
+											x={200}
+											y={120}
+											rpm={
+												carTelemetryData[
+													carTelemetryData.length - 1
+												].EngineRPM
+											}
+											gear={
+												carTelemetryData[
+													carTelemetryData.length - 1
+												].Gear
+											}
+										/>
+									</Col>
+									<Col
+										xs={6}
+										// style={{ border: "1px solid blue" }}
+									>
+										<SpeedChart
+											x={80}
+											y={60}
+											value={
+												carTelemetryData[
+													carTelemetryData.length - 1
+												].Speed
+											}
+										/>
+										<SteeringIndicator
+											value={
+												carTelemetryData[
+													carTelemetryData.length - 1
+												].Steer
+											}
+										/>
+									</Col>
+								</Row>
+								{/* <NumberChart
                                     value={
                                         telemetryData[telemetryData.length - 1]
                                             .speed
@@ -177,81 +186,83 @@ export const LiveData = () => {
                                     }
                                     label="DRS"
                                 /> */}
-                            </>
-                        ) : (
-                            <></>
-                        )}
-                    </div>
-                </Col>
-                <Col>
-                    {carTelemetryData.length >= 1 ? (
-                        <>
-                            {/* {typeof telemetryData[telemetryData.length - 1]
+							</>
+						) : (
+							<></>
+						)}
+					</div>
+				</Col>
+				<Col>
+					{carTelemetryData.length >= 1 ? (
+						<>
+							{/* {typeof telemetryData[telemetryData.length - 1]
                                 .TyresSurfaceTemperature !== "undefined"
                                 ? console.log(
                                       telemetryData[telemetryData.length - 1]
                                           .TyresSurfaceTemperature
                                   )
                                 : console.log("Not Init")} */}
-                            <TyreChartAlternate
-                                tireData={{
-                                    telemetryData:
-                                        carTelemetryData[
-                                            carTelemetryData.length - 1
-                                        ],
-                                    statusData:
-                                        carStatusData.length > 0
-                                            ? carStatusData[
-                                                  carStatusData.length - 1
-                                              ]
-                                            : 0,
-                                }}
-                            />
-                            <Row>
-                                <Col xs={4}>
-                                    <PitChart
-                                        x={100}
-                                        y={10}
-                                        pitRec1={9}
-                                        pitRec2={12}
-                                    />
-                                </Col>
-                                <Col xs={4}>
-                                    <FuelChart
-                                        x={100}
-                                        y={10}
-                                        value={
-                                            carStatusData.length > 0
-                                                ? carStatusData[
-                                                      carStatusData.length - 1
-                                                  ].FuelRemainingLaps
-                                                : 0
-                                        }
-                                    />
-                                </Col>
-                                <Col xs={4}>
-                                    <LiveBestLapTimes
-                                        x={100}
-                                        y={10}
-                                        personalBest={"1:09:23"}
-                                        sessionBest={"1:09:23"}
-                                    />
-                                </Col>
-                            </Row>
-                        </>
-                    ) : (
-                        <></>
-                    )}
-                </Col>
-            </Row>
-            <Row>
-                <Col style={{ marginLeft: "20px" }}>
-                    <LapDetailsTracker
-                        incomingData={lapData[lapData.length - 1]}
-                    />
-                </Col>
-                <Col></Col>
-            </Row>
-        </div>
-    )
-}
+							<TyreChartAlternate
+								tireData={{
+									telemetryData:
+										carTelemetryData[
+											carTelemetryData.length - 1
+										],
+									statusData:
+										carStatusData.length > 0
+											? carStatusData[
+													carStatusData.length - 1
+											  ]
+											: 0,
+								}}
+							/>
+							<Row>
+								<Col xs={4}>
+									<PitChart
+										x={100}
+										y={10}
+										pitRec1={9}
+										pitRec2={12}
+									/>
+								</Col>
+								<Col xs={4}>
+									<FuelChart
+										x={100}
+										y={10}
+										value={
+											carStatusData.length > 0
+												? carStatusData[
+														carStatusData.length - 1
+												  ].FuelRemainingLaps
+												: 0
+										}
+									/>
+								</Col>
+								<Col xs={4}>
+									<LiveBestLapTimes
+										x={100}
+										y={10}
+										personalBest={"1:09:23"}
+										sessionBest={"1:09:23"}
+									/>
+								</Col>
+							</Row>
+						</>
+					) : (
+						<></>
+					)}
+				</Col>
+			</Row>
+			<Row>
+				<Col style={{ marginLeft: "20px" }}>
+					{lapData.length >= 1 && (
+						<LapDetailsTracker
+							incomingData={lapData[lapData.length - 1]}
+						/>
+					)}
+				</Col>
+				<Col></Col>
+			</Row>
+		</div>
+	);
+};
